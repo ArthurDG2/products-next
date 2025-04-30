430149b64a880689558585026df9ad06da33455a
ADGomes <arthurdaigomes@gmail.com>
2025-04-30 09:23:52 -0300

    Aula 30-04

--- src/app/products/api.ts
+++ src/app/products/api.ts
@@ -12,13 +12,20 @@ export async function getProducts(): Promise<IProduct[]>{
 export async function createProduct(data: IProductRequest){
     const response = await fetch(apiUrl, {
         method: 'POST',
-        headers:{
-            "Content-Type" : "aplication/json"
+        headers: {
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(data)
     })
 
     if(response.status == 201){
-        alert('Produto cadastrado com sucesso')
+        alert('Produto cadastrado com sucesso!')
     }
+}
+
+export async function getOneProductById(id: string): Promise<IProduct | null>{
+    const response = await fetch(apiUrl+'/'+id)
+    if(response.status != 200) return null
+    const json = await response.json()
+    return json
 }
